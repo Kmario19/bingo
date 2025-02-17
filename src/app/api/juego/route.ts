7c928c26e62dc0c1b0d6241d8092d77fe97f99ce
Kmario19 <kmario1019@gmail.com>
2025-02-16 22:01:24 -0500

    refactor: simplify API route methods by removing response parameter and updating response handling

--- src/app/api/juego/route.ts
+++ src/app/api/juego/route.ts
@@ -1,17 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
-interface Juego {
-  id: string;
-  title: string;
-  dateStart: Date;
-  dateEnd: Date;
-  numberOfCards: number;
-}
+import { Juego } from '@/types/juego';
 
 let juegos: Juego[] = [];
 
 // Create a new juego
-export async function POST(request: Request, response: Response) {
+export async function POST(request: Request) {
   const { title, dateStart, dateEnd, numberOfCards } = await request.json();
   const newJuego: Juego = {
     id: uuidv4(),
@@ -21,16 +15,16 @@ export async function POST(request: Request, response: Response) {
     numberOfCards,
   };
   juegos.push(newJuego);
-  return response.json(newJuego);
+  return Response.json(newJuego);
 }
 
 // Read all juegos
-export async function GET(request: Request, response: Response) {
-  response.json(juegos);
+export async function GET() {
+  Response.json(juegos);
 }
 
 // Update a juego by ID
-export async function PUT(request: Request, response: Response) {
+export async function PUT(request: Request) {
   const { id, title, dateStart, dateEnd, numberOfCards } = await request.json();
   const juegoIndex = juegos.findIndex((j) => j.id === id);
   if (juegoIndex !== -1) {
@@ -41,15 +35,15 @@ export async function PUT(request: Request, response: Response) {
       dateEnd: new Date(dateEnd),
       numberOfCards,
     };
-    return response.json(juegos[juegoIndex]);
+    return Response.json(juegos[juegoIndex]);
   } else {
-    return response.json({ message: 'Juego not found' });
+    return Response.json({ message: 'Juego not found' });
   }
 }
 
 // Delete a juego by ID
-export async function DELETE(request: Request, response: Response) {
+export async function DELETE(request: Request) {
   const { id } = await request.json();
   juegos = juegos.filter((j) => j.id !== id);
-  return response.json();
+  return Response.json(juegos);
 }
