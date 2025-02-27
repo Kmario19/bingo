92180414f163460f8976d81de29a6dad101e1357
Kmario19 <kmario1019@gmail.com>
2025-02-26 19:17:36 -0500

    feat: update game model to include maxPlayers and adjust API routes

--- src/app/api/juego/route.ts
+++ src/app/api/juego/route.ts
@@ -1,18 +1,20 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { Juego } from '@/types/juego';
+import { Game } from '@/types/game';
 
-let juegos: Juego[] = [];
+let juegos: Game[] = [];
 
 // Create a new juego
 export async function POST(request: Request) {
-  const { title, dateStart, dateEnd, numberOfCards } = await request.json();
-  const newJuego: Juego = {
+  const { title, dateStart, dateEnd, maxPlayers, numberOfCards } =
+    await request.json();
+  const newJuego: Game = {
     id: uuidv4(),
     title,
     dateStart: new Date(dateStart),
     dateEnd: new Date(dateEnd),
     numberOfCards,
+    maxPlayers,
   };
   juegos.push(newJuego);
   return Response.json(newJuego);
@@ -25,7 +27,8 @@ export async function GET() {
 
 // Update a juego by ID
 export async function PUT(request: Request) {
-  const { id, title, dateStart, dateEnd, numberOfCards } = await request.json();
+  const { id, title, dateStart, dateEnd, maxPlayers, numberOfCards } =
+    await request.json();
   const juegoIndex = juegos.findIndex((j) => j.id === id);
   if (juegoIndex !== -1) {
     juegos[juegoIndex] = {
@@ -34,6 +37,7 @@ export async function PUT(request: Request) {
       dateStart: new Date(dateStart),
       dateEnd: new Date(dateEnd),
       numberOfCards,
+      maxPlayers,
     };
     return Response.json(juegos[juegoIndex]);
   } else {
